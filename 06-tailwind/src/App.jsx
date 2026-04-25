e86a573cd0aea11051cb73b3a65f84554075765d
CODEBOT <pankoustav@gmail.com>
2026-04-25 19:44:12 +0530

    Ui Project Start

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-600 h-screen flex items-center justify-center text-white text-3xl font-bold'>
+      App
+    </div>
+  )
+}
+
+export default App
